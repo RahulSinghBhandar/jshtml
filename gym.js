9764474b7e28c3d5b7b8afcea21a6fcc22837bb1
RahulSinghBhandar <141235168+RahulSinghBhandar@users.noreply.github.com>
2024-11-16 20:58:28 -0800

    gym

---
+++ gym.js
@@ -0,0 +1,74 @@
+function home(){
+    document.getElementById("homes").style.color="rgb(20, 251, 20)";
+    document.getElementById("plans").style.color="white";
+    document.getElementById("contacts").style.color="white";
+    document.getElementById("blogs").style.color="white";
+    document.getElementById("programs").style.color="white";
+    document.getElementById("trainer").style.color="white";
+
+
+}
+
+function program(){
+    document.getElementById("homes").style.color="white";
+    document.getElementById("plans").style.color="white";
+    document.getElementById("contacts").style.color="white";
+    document.getElementById("blogs").style.color="white";
+    document.getElementById("programs").style.color="rgb(20, 251, 20)";
+    document.getElementById("trainer").style.color="white";
+
+
+}
+function plan(){
+    document.getElementById("homes").style.color="white";
+    document.getElementById("plans").style.color="rgb(20, 251, 20)";
+    document.getElementById("contacts").style.color="white";
+    document.getElementById("blogs").style.color="white";
+    document.getElementById("programs").style.color="white";
+    document.getElementById("trainer").style.color="white";
+
+
+}
+function blog(){
+    document.getElementById("homes").style.color="white";
+    document.getElementById("plans").style.color="white";
+    document.getElementById("contacts").style.color="white";
+    document.getElementById("blogs").style.color="rgb(20, 251, 20)";
+    document.getElementById("programs").style.color="white";
+    document.getElementById("trainer").style.color="white";
+
+
+}
+function trainer(){
+    document.getElementById("homes").style.color="white";
+    document.getElementById("plans").style.color="white";
+    document.getElementById("contacts").style.color="white";
+    document.getElementById("blogs").style.color="rgb(20, 251, 20)";
+    document.getElementById("programs").style.color="white";
+    document.getElementById("trainer").style.color="white";
+}
+function contact(){
+    document.getElementById("homes").style.color="white";
+    document.getElementById("plans").style.color="white";
+    document.getElementById("contacts").style.color="rgb(20, 251, 20)";
+    document.getElementById("blogs").style.color="white";
+    document.getElementById("programs").style.color="white";
+    document.getElementById("trainer").style.color="white";
+
+
+}
+
+// submit
+
+function submit(){
+    let name = document.getElementById("name");
+    let number = document.getElementById("number");
+
+    if(name.value == ""){
+        alert("Please Enter Name")
+    }else if(number.value == ""){
+        alert("Please Enter Number")
+    }else{
+        alert("we will connect with you soon: " + name.value)
+    }
+}
